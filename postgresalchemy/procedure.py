184ee9688ba84c9eacd06776b6a1d966d0bce193
Nathan Rice <nathan_rice@unc.edu>
2016-08-02 17:50:24 -0400

    More updates to procedure

--- postgresalchemy/procedure.py
+++ postgresalchemy/procedure.py
@@ -1,8 +1,9 @@
 import inspect
 import zlib
-import typing
+import math
 import sqlalchemy
 import re
+from typing import TypeVar
 
 from .types import *
 
@@ -14,15 +15,15 @@ class Procedure(object):
         $$ LANGUAGE plpython3u;
     """
 
-    def __init__(self, name=None, parameters="", return_type="void", code=""):
-        if not name:
-            self.name = "procedure_" + str(abs(zlib.adler32(code)))
-        self.parameters = parameters
+    def __init__(self, name=None, parameters=None, return_type="void", code=""):
+        self.name = name if name is not None else "procedure_" + str(abs(zlib.adler32(code)))
+        self.parameters = parameters if parameters is not None else []
         self.return_type = return_type
         self.code = code
 
     def __str__(self):
-        return self._sql_template.format(name=self.name, parameters=self.parameters, return_type=self.return_type,
+        parameters = ", ".join(self.parameters)
+        return self._sql_template.format(name=self.name, parameters=parameters, return_type=self.return_type,
                                          code=self.code)
 
 
@@ -34,11 +35,11 @@ class ProcedureGenerator(object):
     @classmethod
     def from_function(cls, f):
         parameters = cls.get_parameters(f)
-        sql_parameters = ", ".join(cls.convert_python_type_to_sql(p) for p in parameters)
+        sql_parameters = [cls.generate_sql_procedure_parameter(p) for p in parameters]
         sql_return = cls.generate_return_type(f)
         # Code related
         function_body = cls.get_function_body(f)
-        cls.check_for_overwritten_input_parameters(function_body)
+        cls.check_for_overwritten_input_parameters(parameters, function_body)
         return Procedure(name=f.__name__, parameters=sql_parameters, return_type=sql_return, code=function_body)
 
     @staticmethod
@@ -64,11 +65,12 @@ class ProcedureGenerator(object):
 
     @classmethod
     def convert_python_type_to_sql(cls, python_type):
-        if isinstance(python_type, sqlalchemy.Table):
-            type_name = python_type.name
-        elif hasattr(python_type, "__table__"):
+        is_class = inspect.isclass(python_type)
+        if hasattr(python_type, "__table__"):
             type_name = python_type.__table__.name
-        elif issubclass(python_type, Array):
+        elif isinstance(python_type, str):
+            type_name = python_type
+        elif is_class and issubclass(python_type, Array):
             type_name = cls.convert_python_type_to_sql(python_type.__args__[0]) + "[]"
         else:
             type_name = mappings.get(python_type)
@@ -77,8 +79,9 @@ class ProcedureGenerator(object):
         return type_name
 
     @classmethod
-    def generate_sql_default_value(cls, type_name, default=None):
-        if default is not None:
+    def generate_sql_default_value(cls, type_name, default=float('nan')):
+        # Using NaN here instead of None since None is a default value you might actually use
+        if not (isinstance(default, float) and math.isnan(default)):
             default_parameters = type_name, cls.convert_python_value_to_sql(default)
             return " DEFAULT ".join(default_parameters)
         else:
@@ -88,15 +91,26 @@ class ProcedureGenerator(object):
     def convert_python_value_to_sql(value):
         def convert_inner(val):
             if val is None:
-                return "NULL"
-            elif isinstance(val, (str, int, float, bool)):
-                return str(val)
+                converted_val = "NULL"
+            elif isinstance(val, str):
+                converted_val = '"%s"' % val.replace('"', '\\"')
+            elif isinstance(val, (int, float, bool)):
+                converted_val = str(val)
             elif isinstance(val, (datetime, date, time)):
-                return val.isoformat()
+                converted_val = val.isoformat()
             elif isinstance(val, Sequence):
-                array_values = ', '.join(convert_inner(v) for v in val)
-                return "{%s}" % array_values
-        result = "'%s'" % convert_inner(value)
+                array_values = ','.join(convert_inner(v) for v in val)
+                converted_val = "{%s}" % array_values
+            elif hasattr(val, "__table__"):
+                converted_val = val.__table__.name
+            else:
+                raise ValueError("No known type mapping for Python type: %s" % type(val))
+            return converted_val
+        if isinstance(value, str):
+            inner = value.replace("'", "\\'")
+        else:
+            inner = convert_inner(value)
+        result = "E'%s'" % inner
         return result
 
     @classmethod
--- test/test_procedure.py
+++ test/test_procedure.py
@@ -1,6 +1,17 @@
-import typing
 import pytest
+from sqlalchemy import MetaData, Table, Column, Integer, Text
+from sqlalchemy.ext.declarative import declarative_base
 import postgresalchemy.procedure as p
+
+metadata = MetaData()
+Base = declarative_base(metadata=metadata)
+test_table = Table("test_table", metadata, Column("id", Integer, primary_key=True), Column("name", Text))
+
+
+class TestMappedClass(Base):
+    __table__ = test_table
+
+mapped_instance = TestMappedClass(id=1, name="test")
 
 
 def example_1():
@@ -16,15 +27,19 @@ def example_3(a: int = 1) -> bool:
 
 
 def example_4(a: int = 1, b: bool = True,
-              c: typing.Sequence[int] = (1, 2, 3)) -> bool:  # test
+              c: p.Array[int] = (1, 2, 3)) -> bool:  # test
     return True
 
 
 def example_5(a: int = 1, b: bool = True,
-              c: typing.Sequence[int] = (1, 2, 3)) -> typing.Sequence[int]:  # test
+              c: p.Array[int] = (1, 2, 3)) -> p.Sequence[int]:  # test
     a = 2
     b = False
     return c
+
+
+def example_6(a: TestMappedClass) -> p.Sequence[TestMappedClass]:  # test
+    return 1, 2, 3
 
 
 def test_get_function_body_simple():
@@ -58,12 +73,12 @@ def test_get_function_body_with_multiline_parameter_and_multiline_body():
 
 def test_check_for_overwritten_input_parameters_should_pass():
     body = p.ProcedureGenerator.get_function_body(example_4)
-    parameters = p.ProcedureGenerator.get_parameters(example_5)
+    parameters = p.ProcedureGenerator.get_parameters(example_4)
     p.ProcedureGenerator.check_for_overwritten_input_parameters(parameters, body)
     assert True
 
 
-def test_check_for_overwritten_input_parameters_should_pass():
+def test_check_for_overwritten_input_parameters_with_exception():
     body = p.ProcedureGenerator.get_function_body(example_5)
     parameters = p.ProcedureGenerator.get_parameters(example_5)
     with pytest.raises(ValueError):
@@ -75,12 +90,25 @@ def test_convert_python_type_to_sql_simple():
     assert p.ProcedureGenerator.convert_python_type_to_sql(p.datetime) == "timestamp without time zone"
 
 
+def test_convert_python_type_to_sql_string_passthrough():
+    assert p.ProcedureGenerator.convert_python_type_to_sql("boolean") == "boolean"
+
+
 def test_convert_python_type_to_sql_array():
     assert p.ProcedureGenerator.convert_python_type_to_sql(p.Array[bool]) == "boolean[]"
 
 
 def test_convert_python_type_to_sql_2d_array():
     assert p.ProcedureGenerator.convert_python_type_to_sql(p.Array[p.Array[bool]]) == "boolean[][]"
+
+
+def test_convert_python_type_to_sql_sqlalchemy_class():
+    assert p.ProcedureGenerator.convert_python_type_to_sql(p.Array[TestMappedClass]) == "test_table[]"
+
+
+def test_convert_python_type_to_sql_notfound():
+    with pytest.raises(ValueError):
+        p.ProcedureGenerator.convert_python_type_to_sql(p.Sequence)
 
 
 def test_generate_return_type_void():
@@ -96,38 +124,65 @@ def test_generate_return_type_sequence():
 
 
 def test_convert_value_to_sql_none():
-    assert p.ProcedureGenerator.convert_python_value_to_sql(None) == "'NULL'"
+    assert p.ProcedureGenerator.convert_python_value_to_sql(None) == "E'NULL'"
 
 
 def test_convert_value_to_sql_boolean():
-    assert p.ProcedureGenerator.convert_python_value_to_sql(True) == "'True'"
+    assert p.ProcedureGenerator.convert_python_value_to_sql(True) == "E'True'"
 
 
 def test_convert_value_to_sql_boolean():
-    assert p.ProcedureGenerator.convert_python_value_to_sql(1.0) == "'1.0'"
+    assert p.ProcedureGenerator.convert_python_value_to_sql(1.0) == "E'1.0'"
 
 
 def test_convert_value_to_sql_datetime():
     now = p.datetime.utcnow()
     date_string = p.ProcedureGenerator.convert_python_value_to_sql(now)
-    assert date_string == "'%s'" % now.isoformat()
+    assert date_string == "E'%s'" % now.isoformat()
 
 
 def test_convert_value_to_sql_array():
     my_array = [1, 2, 3]
     array_string = p.ProcedureGenerator.convert_python_value_to_sql(my_array)
-    assert array_string == "'{1, 2, 3}'"
+    assert array_string == "E'{1,2,3}'"
 
 
 def test_convert_value_to_sql_array_2d():
     my_array = [[1, 2], [3, 4]]
     array_string = p.ProcedureGenerator.convert_python_value_to_sql(my_array)
-    assert array_string == "'{{1, 2}, {3, 4}}'"
+    assert array_string == "E'{{1,2},{3,4}}'"
+
+
+def test_convert_python_value_to_sql_unknown():
+    with pytest.raises(ValueError):
+        p.ProcedureGenerator.convert_python_value_to_sql(complex)
 
 
 def test_generate_sql_default_value_nodefault():
+    type_and_default = p.ProcedureGenerator.generate_sql_default_value("boolean")
+    assert type_and_default == "boolean"
+
+
+def test_generate_sql_default_value_boolean_default():
     type_and_default = p.ProcedureGenerator.generate_sql_default_value("boolean", True)
-    assert type_and_default == "boolean DEFAULT 'True'"
+    assert type_and_default == "boolean DEFAULT E'True'"
+
+
+def test_generate_sql_default_value_null_default():
+    type_and_default = p.ProcedureGenerator.generate_sql_default_value("boolean", None)
+    assert type_and_default == "boolean DEFAULT E'NULL'"
+
+
+def test_generate_sql_default_value_float_default():
+    type_and_default = p.ProcedureGenerator.generate_sql_default_value("float", 1.0)
+    assert type_and_default == "float DEFAULT E'1.0'"
+
+
+def test_generate_procedure_from_function():
+    procedure = p.ProcedureGenerator.from_function(example_4)
+    assert procedure.name == "example_4"
+    assert procedure.parameters == ["a int DEFAULT E'1'", "b boolean DEFAULT E'True'", "c int[] DEFAULT E'{1,2,3}'"]
+    assert procedure.return_type == "boolean"
 
 
 if __name__ == "__main__":
